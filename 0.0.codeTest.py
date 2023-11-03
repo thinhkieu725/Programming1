0899162a4967d37dc3b4809b7c53bfe22a8b1b85
thinhkieu725 <thinhkieu725@gmail.com>
2023-11-03 20:45:21 +0200

    03Nov2023_20h45

--- 0.0.codeTest.py
+++ 0.0.codeTest.py
@@ -10,14 +10,38 @@ This is the file for testing
 from tkinter import *
 
 root = Tk()
+root.geometry("300x300")
 
-def hello():
-    print("hello!")
+w = Label(root, text='StudyTonight', fg="Blue", font="100")
+w.pack()
 
-menubar = Menu(root)
-menubar.add_command(label="Hello StudyTonight!", command=hello)
-menubar.add_command(label="Quit!", command=root.quit)
+Checkbutton1 = IntVar()
+Checkbutton2 = IntVar()
+Checkbutton3 = IntVar()
 
-root.config(menu=menubar)
+Button1 = Checkbutton(root, text="Homepage",
+                      variable=Checkbutton1,
+                      onvalue=1,
+                      offvalue=0,
+                      height=2,
+                      width=10)
 
-root.mainloop()
+Button2 = Checkbutton(root, text="Tutorials",
+                      variable=Checkbutton2,
+                      onvalue=1,
+                      offvalue=0,
+                      height=2,
+                      width=10)
+
+Button3 = Checkbutton(root, text="Contactus",
+                      variable=Checkbutton3,
+                      onvalue=1,
+                      offvalue=0,
+                      height=2,
+                      width=10)
+
+Button1.pack()
+Button2.pack()
+Button3.pack()
+
+mainloop()
