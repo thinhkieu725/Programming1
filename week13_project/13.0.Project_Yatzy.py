af57bc538e89445dbfe167ff0490d4ceb06e83a1
Thinh Kieu <124564132+thinhkieu725@users.noreply.github.com>
2023-11-04 14:53:29 +0200

    04Nov2023_14h53

--- week13_project/13.0.Project_Yatzy.py
+++ week13_project/13.0.Project_Yatzy.py
@@ -15,11 +15,22 @@ from tkinter import *
 
 class Yatzy:
     def __init__(self):
-        pass
+        # INITIALIZE THE WINDOW AND IMAGES
+
+        # Initialize the root window
+        self.__main_window = Tk()
+        self.__main_window.title("Yatzy")
+
+        # Create PhotoImage objects and store them in a list
+
+        # Create an empty PhotoImage object with the size of the dice
+
+        # ============================================================
 
 
 def main():
     Yatzy()
+
 
 if __name__ == "__main__":
     main()
