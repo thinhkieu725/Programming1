f9fb670694b7bfe9861ec181ab2c164c0012da96
thinhkieu725 <thinhkieu725@gmail.com>
2023-11-24 18:08:59 +0200

    24Nov2023_18h08

--- week12/12.3.Project_Battleship.py
+++ week12/12.3.Project_Battleship.py
@@ -7,23 +7,38 @@ Student ID number: 152167613
 Student ID: dxthki
 Email: thinh.kieu@tuni.fi
 
-# TODO: Write program description.
+This program establishes a single-player version of the game Battleship.
+The player's objective is to sink all enemy (computer's) ship
 """
 
 # Constants storing standard coordinate values for reference.
 # X represent columns, Y represent rows.
 X_COORDINATES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 Y_COORDINATES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# All marks that denote sunken ships on the board are stored in a dictionary.
+SHIP_MARKS = {"battleship": "B", "cruiser": "C", "destroyer": "D",
+              "submarine": "S"}
 
 # All the ships in the game are stored in a list.
 ships = []
 # Establish a nested structure to store the 10x10 game board.
-board = [[" "] * 10] * 10
+board = [
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+]
 
 
 class Ship():
     """
-    # TODO: Add class description.
+    This class stores the information and methods of every ship on the board.
     """
 
     def __init__(self, ship_type, ship_coordinate):
@@ -38,10 +53,46 @@ class Ship():
         self.__ship_coordinate = ship_coordinate
         self.__sunken = False
 
+    def get_sunken(self):
+        """
+        Return the state of the ship.
+        :return: bool, True if the ship is sunken
+        """
+        return self.__sunken
+
     def does_occupy(self, coordinate):
-        # TODO: Write function description.
-        # TODO: Implement the function.
-        pass
+        """
+        Check whether a ship occupies a square.
+        :param coordinate: str, the prompted coordinate
+        :return: bool, True if the ship occupies the square
+        """
+        coordinate = coordinate.upper()
+        if coordinate in self.__ship_coordinate:
+            return True
+        else:
+            return False
+
+    def update_state(self):
+        """
+        Check if all squares of a ship is attacked.
+        If so, update __sunken to true, update the mark on the board
+        and print out the notification.
+        """
+        # Loop through every square that the ship occupies.
+        for coord in self.__ship_coordinate:
+            x, y = convert_coordinate(coord)
+            # If the square has not been shot at, the ship is not sunken.
+            # Stop the process
+            if board[y][x] == " ":
+                return
+        # If the ship is sunken, update the state.
+        self.__sunken = True
+        # Update the mark.
+        for coord in self.__ship_coordinate:
+            x, y = convert_coordinate(coord)
+            board[y][x] = SHIP_MARKS[self.__ship_type]
+        # Print out the notification.
+        print(f"You sank a {self.__ship_type}!")
 
 
 def convert_coordinate(coordinate):
@@ -62,8 +113,14 @@ def is_occupied_by(coordinate):
     :param coordinate: the prompted square
     :return: int, index of the ship that occupies the square; -1 if unoccupied.
     """
-    # TODO: Implement the function.
-    pass
+    coordinate = coordinate.upper()
+    # Loop through all ships using index.
+    for idx in range(len(ships)):
+        # If the ship occupies the square, return the index.
+        if ships[idx].does_occupy(coordinate):
+            return idx
+    # If no ship occupies the square, return -1
+    return -1
 
 
 def print_board():
@@ -75,6 +132,7 @@ def print_board():
     print("  ", end="")
     for c in X_COORDINATES:
         print(c, end=" ")
+    print()
     # Print 10 main rows.
     for y_coord in range(10):
         print(y_coord, end=" ")
@@ -86,6 +144,7 @@ def print_board():
     for c in X_COORDINATES:
         print(c, end=" ")
     print()
+    print()
 
 
 def valid_coordinate(coordinate):
@@ -95,10 +154,24 @@ def valid_coordinate(coordinate):
     :return: bool, True if the coordinate is valid.
     """
     coordinate = coordinate.upper()
+    # Check if the string has the right length.
     if len(coordinate) != 2:
         return False
-    if not (coordinate[1] in X_COORDINATES and coordinate[2] in Y_COORDINATES):
+    # Check if the characters are valid coordinates.
+    if not (coordinate[0] in X_COORDINATES and coordinate[1] in Y_COORDINATES):
         return False
+    return True
+
+
+def win():
+    """
+    Check whether the player has won, i.e. all ships has been sunken.
+    :return: bool, True if all ships are sunken
+    """
+    # Loop through all ships and check their state.
+    for s in ships:
+        if s.get_sunken() == False:
+            return False
     return True
 
 
@@ -106,6 +179,8 @@ def main():
     # ========== PROCESSING FILE DATA ==========
     # Prompt the user to enter the file and then read the file.
     file_name = input("Enter file name: ")
+    if file_name == "":
+        file_name = "input.txt"
     try:
         file_handle = open(file_name, "r")
     except OSError:
@@ -113,14 +188,14 @@ def main():
         return
     # Process the file line by line.
     for file_line in file_handle:
-        components = file_line.split(";")
+        components = file_line.strip().split(";")
         # Extract ship_type and ship_coordinates.
         ship_type = components[0]
-        ship_coordinates = []
+        new_ship_coordinates = []
         for coord in components[1:]:
-            ship_coordinates.append(coord.upper())
+            new_ship_coordinates.append(coord.upper())
         # Check whether the new ship data is valid.
-        for coord in ship_coordinates:
+        for coord in new_ship_coordinates:
             if not valid_coordinate(coord):
                 print("Error in ship coordinates!")
                 return
@@ -128,7 +203,7 @@ def main():
                 print("There are overlapping ships in the input file!")
                 return
         # Add the new ship to the list.
-        ships.append(Ship(ship_type, ship_coordinates))
+        ships.append(Ship(ship_type, new_ship_coordinates))
 
     # ========== GAME RUNNING ==========
     print_board()
@@ -154,13 +229,22 @@ def main():
             print_board()
             continue
 
+        attacked_ship = is_occupied_by(prompt)
         # If the shot does not hit any ship.
-        if is_occupied_by(prompt) == -1:
+        if attacked_ship == -1:
             board[y_coord][x_coord] = "*"
         # If the shot hits a ship.
         else:
-
-
+            # Mark X at the attacked square
+            board[y_coord][x_coord] = "X"
+            # Run update_state function for the attacked ship
+            ships[attacked_ship].update_state()
+        print_board()
+        # At the end of each round in which a ship it hit,
+        # check whether the player has won the game.
+        if win():
+            print("Congratulations! You sank all enemy ships.")
+            break
 
 
 if __name__ == "__main__":
